efbf06b911438aedcc9a6f22581baf639aed4e91
Jose Uriel Rodriguez Ramirez <uri_055@hotmail.com>
2024-06-26 15:45:14 -0600

    Adicion de diseño de loader

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -3,7 +3,20 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'beat-loader',
   templateUrl: './loader.component.html',
-  styles: []
+  styles: [`
+        .full-screen-overlay {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(0, 0, 0, 0.5); /* Negro con transparencia del 50% */
+          z-index: 9999; /* Asegura que esté por encima de otros elementos */
+          display: flex;
+          justify-content: center;
+          align-items: center;
+    }
+    `]
 })
 export class LoaderComponent {
 
